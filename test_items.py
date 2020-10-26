802570a6fb27476ec41f2fae04e8a21ff967c3f5
alexander <sasha.fursof@gmail.com>
2020-10-26 21:19:53 +0700

    test added

---
+++ test_items.py
@@ -0,0 +1,7 @@
+import time
+
+def test_test_find_add_to_card_btn(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    time.sleep(10)
+    assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(), \
+        'Кнопка добавления товара в корзину отсутсвует'
